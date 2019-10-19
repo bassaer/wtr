19c9f482f9d9b7c4f237a948edc118d0c750709a
Tsubasa Nakayama <app.nakayama@gmail.com>
2019-10-20 01:32:50 +0900

    add node-emoji

--- src/wtr.ts
+++ src/wtr.ts
@@ -1,3 +1,36 @@
-export const wtr = () => {
-    return "wtr!";
+import emoji from "node-emoji";
+import { tsConstructorType } from "@babel/types";
+
+enum Status {
+    Sunny,
+    Cloudy,
+    Rainy 
 };
+
+export class Weather {
+    private status: Status
+
+    constructor(status: Status) {
+        this.status = status;
+    }    
+    
+    toString() {
+        switch(this.status) {
+            case Status.Sunny:
+                return emoji.get("sunny");
+            case Status.Cloudy:
+                return emoji.get("cloud");
+            case Status.Rainy:
+                return emoji.get("umbrella_with_rain_drops");
+        }
+    }
+};
+
+export const wtr = (status: Status) => {
+    const weather = new Weather(status);
+    console.log(weather.toString());
+};
+
+wtr(Status.Sunny)
+wtr(Status.Cloudy)
+wtr(Status.Rainy)
